a1f35ba22123d270b7cafcc31c290fbd37560083
codeprnv <pranavpradhan23424@gmail.com>
2025-06-21 22:48:26 +0530

    Updated basics

--- basic_1.py
+++ basic_1.py
@@ -6,9 +6,10 @@ def count_digits(n):
     import math
     
     total = int(math.log10(n)) + 1
-    print(f'Total no. of digits in {n} are: {total}')
+    return total
 
-count_digits(n)
+# digits = count_digits(n)
+# print(f'Total no. of digits in {n} are: {digits}')
 
 # 2. Problem Statement: Given an integer N return the reverse of the given number.
 
@@ -21,7 +22,7 @@ def reverse_number(n):
         n = n // 10
     print(f'Reverse number of {num} is {rev}')
 
-reverse_number(n)
+# reverse_number(n)
 
 #  3. Problem Statement: Given an integer N, return true if it is a palindrome else return false.
 
@@ -37,7 +38,7 @@ def check_palindrome(n):
     else:
         print(f'The number {num} is not palindrome')
     
-check_palindrome(n)
+# check_palindrome(n)
 
 
 #  4. Problem Statement: Given two integers N1 and N2, find their greatest common divisor.
@@ -51,7 +52,56 @@ def find_gcd(a: int, b: int):
         return b 
     return a
 
-a = int(input('Enter first number: '))
-b = int(input('Enter second number: '))
-gcd = find_gcd(a, b)
-print(f'GCD of {a} and {b} is: {gcd}')
+# a = int(input('Enter first number: '))
+# b = int(input('Enter second number: '))
+# gcd = find_gcd(a, b)
+# print(f'GCD of {a} and {b} is: {gcd}')
+
+#  5. Problem Statement: Given an integer N, return true it is an Armstrong number otherwise return false.
+
+def armstrong_number(n):
+    k = len(str(n))
+    sum = 0
+    num = n
+    while n > 0:
+        temp = n % 10
+        sum += temp ** k
+        n = n // 10
+    if(sum == num):
+        print(f'{num} is an armstrong number')
+    else:
+        print(f'{num} is not an armstrong number')
+
+# armstrong_number(n)
+
+#  6. Problem Statement: Given an integer N, return true it is an Prime number otherwise return false.
+
+def check_prime(n):
+    import math
+    sqrt = int(math.sqrt(n))
+    counter = 0
+    for i in range(1, sqrt + 1):
+        if(n % i == 0):
+            counter += 1
+        if(i != n // i):
+            counter += 1
+    if(counter == 2):
+        print(f'{n} is prime number')
+    else:
+        print(f'{n} is not a prime number')
+
+# check_prime(n)
+
+#  7. Problem Statement: Given an integer N, return all divisors of N.
+
+def find_divisors(n):
+    import math
+    divisors = list()
+    for i in range(1, int(math.sqrt(n)) + 1):
+        if(n % i == 0):
+            divisors.append(i)
+        if(i != n // i):
+            divisors.append(n // i)
+    print(f'Divisors of {n} are: {sorted(divisors)}')
+    
+find_divisors(n)
